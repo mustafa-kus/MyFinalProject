83007c70c31699c2fde796deda0d23df49baadf5
mustafa kuş <mustafa_kus@hotmail.com>
2021-02-27 22:54:33 +0300

    27 şubat 2.ara

---
+++ Core/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities.Concrete
+{
+    public class OperationClaim
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities.Concrete
+{
+    public class UserOperationClaim
+    {
+        public int id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+}
